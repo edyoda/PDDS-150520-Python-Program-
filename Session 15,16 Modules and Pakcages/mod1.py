069f9c7dc534cbe073dbf9c8b6ea9f46378869b4
Mohit Singh Chouhan <mohitchouhan1947@gmail.com>
2020-06-11 11:28:40 +0530

    Add Session 15

---
+++ Session 15,16 Modules and Pakcages/mod1.py
@@ -0,0 +1,30 @@
+# import sys
+
+def spacex(work_given_to_spacex):
+    ceo = "elon musk"
+    document = [ceo,work_given_to_spacex]
+    # print(globals())
+    # print(locals())
+    return document
+
+def google(work_given_to_google):
+    king = "sundar pichai"
+    t1 = "2342kj"
+    document = [king,work_given_to_google,t1]
+    return document
+
+if __name__=="__main__":
+	print(spacex("land us on mars"))
+
+# name = "rohan"
+# print(spacex("land us on mars"))
+
+# print(google("make an amazing AI"))
+
+
+
+# print("\n")
+
+# print(dir())
+
+# print(globals())
---
+++ Session 15,16 Modules and Pakcages/start.py
@@ -0,0 +1,75 @@
+import mod1
+
+package1(folder) --> mod1(python file),mod2(python file)
+
+
+
+
+
+## keep in mind that your python path is set.
+
+
+
+
+
+# from mod1 import spacex
+# pi = "land us on mars"
+
+# from math import pi as pi2
+
+# import math as mathematics
+
+# print(mathematics.pi)
+
+# print(pi,"  ->>  ",pi2)
+
+# print(dir())
+# print("\n")
+# from mod1 import *
+# print(dir())
+
+# def give_work(given_work):
+# 	from mod1 import spacex
+# 	print(spacex(given_work))
+
+
+# give_work("land us on mars")
+
+# def spacex(work_given_to_spacex):
+#     ceo = "musk"
+#     document = [ceo,work_given_to_spacex]
+    # print(globals())
+    # print(locals())
+    # return document
+
+# from mod1 import spacex
+
+
+
+# print(dir())
+# print("\n")
+# import mod1
+# print(dir())
+
+# print(dir())
+# print("\n")
+# from mod1 import spacex
+# import mod1
+
+# print(mod1.sys.path)
+# import math
+
+# print(mod1.__file__)
+
+# print(math.__file__)
+# print(dir())
+
+# import sys
+
+# print(spacex("land us on mars"))
+
+# print(mod1.google("make an amazing AI"))
+
+# print(sys.path)
+
+# dir()
