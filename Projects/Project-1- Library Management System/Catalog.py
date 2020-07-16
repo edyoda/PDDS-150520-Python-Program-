d8f723bab433ac4685b11a3bc46993d552b9c602
Mohit Singh Chouhan <mohitchouhan1947@gmail.com>
2020-07-16 13:03:36 +0530

    Add Library Management System

---
+++ Projects/Project-1- Library Management System/Catalog.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from Book import Book
+#First Book is file & second is Class
+
+class Catalog:
+    def __init__(self):
+        self.different_book_count = 0
+        self.books = []
+        
+    #Only available to admin
+    def addBook(self,name,author,publish_date,pages):
+        b = Book(name,author,publish_date,pages)
+        self.different_book_count += 1
+        self.books.append(b)
+        return b
+    
+    #Only available to admin
+    def addBookItem(self,book,isbn,rack):
+        book.addBookItem(isbn, rack)
+        
+    def searchByName(self,name):
+        for book in self.books:
+            if name.strip() == book.name:
+                return book
+    
+    def searchByAuthor(self,author):
+        pass
+    
+        
+    def displayAllBooks(self):
+        print ('Different Book Count',self.different_book_count)
+        c = 0
+        for book in self.books:
+            c += book.total_count
+            book.printBook()
+        
+        print ('Total Book Count',c)
+        
+    def removeBookItem(self,name,isbn):
+        book = self.searchByName(name)
+        book_item = book.searchBookItem(isbn)
+        book.removeBookItem(book_item)
